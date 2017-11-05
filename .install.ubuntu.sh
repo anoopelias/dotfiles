2d8daaf42107f2d34377fcba6528c3f0d75a5bff
anoopelias <anoopelias@gmail.com>
2017-11-06 02:42:15 +0530

    Added node installation instructions

--- .install.ubuntu.sh
+++ .install.ubuntu.sh
@@ -6,3 +6,7 @@ sudo apt-get install keepassxc
 
 sudo apt-get install fonts-inconsolata
 
+## For installing Node.js
+# Install n and then follow the instructions here,
+# https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory
+
