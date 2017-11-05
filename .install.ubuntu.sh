a8517954b9793d703542e055fde4e4a0dc5b6681
anoopelias <anoopelias@gmail.com>
2017-11-06 00:00:34 +0530

    Install inconsolata for Ubuntu

--- .install.ubuntu.sh
+++ .install.ubuntu.sh
@@ -3,5 +3,6 @@ sudo apt-get install curl vim git xsel tmux
 
 sudo apt-get install exfat-fust exfat-utils
 sudo apt-get install keepassxc
-sudo apt-get install keepassxc
+
+sudo apt-get install fonts-inconsolata
 
