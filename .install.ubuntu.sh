6bec2618223e807f1495758ef36594ccba271a7b
anoopelias <anoopelias@gmail.com>
2018-01-11 12:27:05 +0530

    Python3 support only for vim

--- .install.ubuntu.sh
+++ .install.ubuntu.sh
@@ -12,13 +12,20 @@ sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
 sudo apt remove vim vim-runtime gvim
 git clone https://github.com/vim/vim.git
 cd vim
+""
+
+# It seems both python and python3 support cannot work together in
+# Ubuntu 16.04. So using only python3 support.
+#
+# Add the following lines for python support
+# --enable-pythoninterp=yes \
+# --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 \
